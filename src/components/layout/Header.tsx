1266d45c7d3ae89899bd8f60c1aed670300c8b11
Thomas-Horvath <thomas.horvath0416@gmail.com>
2025-04-25 19:50:51 +0200

    header announcmentBar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,57 @@
-import React from 'react'
+'use client'
+
+
+
+import React, { useEffect, useState } from 'react'
+
+
+const AnnouncmentBar = () => {
+  return (
+    <div className='w-full bg-black py-2'>
+      <div className='container mx-auto flex items-center justify-center px-8'>
+        <span className='text-center text-sm font-medium tracking-wide text-white'>
+          FREE SHIPPING on orders over $15.00! + FREE RETURNS
+        </span>
+      </div>
+    </div>
+  )
+}
+
+
 
 const Header = () => {
+
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [prevScrollY, setPrevScrollY] = useState<number>(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      const scrolledUp = currentScrollY < prevScrollY;
+
+      if (scrolledUp) {
+        setIsOpen(true);
+      } else if (currentScrollY > 100) {
+        setIsOpen(false);
+      }
+
+      setPrevScrollY(currentScrollY);
+    }
+
+    setPrevScrollY(window.scrollY);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [prevScrollY])
+
+
   return (
-    <div>Header</div>
+    <header className='w-full sticky top-0 z-50'>
+      <div className={`w-full transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-y-0':'-translate-y-full'}`}>
+        <AnnouncmentBar />
+      </div>
+    </header>
   )
 }
 
