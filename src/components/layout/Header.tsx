d420fe9ede5325330283e2bc8e0908e2a8c36e5a
Thomas-Horvath <thomas.horvath0416@gmail.com>
2025-04-27 15:58:58 +0200

    header navigation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,7 @@
 
 
 
+import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 
 
@@ -48,8 +49,75 @@ const Header = () => {
 
   return (
     <header className='w-full sticky top-0 z-50'>
-      <div className={`w-full transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-y-0':'-translate-y-full'}`}>
+      <div className={`w-full transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-y-0' : '-translate-y-full'}`}>
         <AnnouncmentBar />
+
+        <div className="w-full
+                        flex justify-between items-center py-3 
+                        sm:py-4 bg-white/80 shadow-sm border-d border-gray-100
+                        backdrop-blur-sm"
+        >
+
+
+          <div className="flex justify-between items-center container mx-auto px-8">
+
+
+
+            {/*  first item  */}
+            <div className='flex flex-1 justify-start items-center gap-4 sm:gap-6'>
+              <button className='text-gray-700 hover:text-gray-900 md:hidden'>
+                <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5 sm:h-6 sm:w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16M4 18h16' />
+                </svg>
+              </button>
+
+              <nav className='hidden md:flex gap-4 lg:gap-6 text-sm font-medium'>
+                <Link href='#' >Shop</Link>
+                <Link href='#' >New Arrivals</Link>
+                <Link href='#' >Sale</Link>
+
+              </nav>
+
+
+
+
+
+
+            </div>
+
+
+
+            {/* second item */}
+            <Link href='#'>link</Link>
+
+
+
+
+            {/* third item */}
+            <div className='flex flex-1 justify-end items-center gap-2 sm:gap-4'>
+              <button className='text-gray-700 hover:text-gray-900 hidden sm:block'>
+                <svg xmlns='http://www.w3.org/2000/svg' className='h-4 w-4 text-gray-400' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
+                </svg>
+              </button>
+
+              <Link href='/auth/sign-in'>Sign In</Link>
+              <Link href='/auth/sign-up'>Sign Up</Link>
+
+
+              <button className="text-gray-700 hover:text-gray-900 relative">
+                <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5 sm:h-6 sm:w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z' />
+                </svg>
+
+                <span className='absolute -top-1 -right-1 bg-black text-white text-[10px] sm:text-xs w-3.5 h-3.5 sm:w-4 sm:h-4 rounded-full flex justify-center items-center'>0</span>
+              </button>
+            </div>
+
+
+
+          </div>
+        </div>
       </div>
     </header>
   )
