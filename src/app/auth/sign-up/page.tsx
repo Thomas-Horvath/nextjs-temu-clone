435e59cce3876ab0c994db3f41cccce2c4899db1
Thomas-Horvath <thomas.horvath0416@gmail.com>
2025-04-27 16:59:41 +0200

    lucia auth

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,20 @@
+
+import { getCurrentSession } from '@/actions/auth';
+import { redirect } from 'next/navigation';
+import React from 'react';
+
+
+const SignUpPage = async () => {
+
+    const  { user } = await  getCurrentSession();
+
+    if(user ) {
+        return redirect('/');
+    }
+
+  return (
+    <div>SignUpPage</div>
+  )
+}
+
+export default SignUpPage
